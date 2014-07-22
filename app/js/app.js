6bc51cd39f6cdaf4c2853796b30af5b86f696892
Robin Berjon <robin@berjon.com>
2014-07-22 13:15:26 +0200

    routing finally works, using a simpler system that's more adapted to node-webkit than the usual Web stuff

--- app/js/app.js
+++ app/js/app.js
@@ -1,71 +1,60 @@
-/*globals page*/
 
 var scrapers = require("../lib/scrapers")
-,   partials = {}
+,   fs = require("fs")
+,   pth = require("path")
 ,   $app
+,   routes = {
+        scraper:        {}
+    ,   documents:      {}
+    ,   "journal-tocs": {}
+    }
 ;
 
-// load up all partials at start-up, *before* doing anything to the routing
-
-// simple partials handling
-function setView (html, cb) {
-    console.log("setting up view");
-    $app.html(html);
-    if (cb) cb();
+// in-app navigation
+function loadPartial (name) {
+    if (!routes[name]) return;
+    if (routes[name].partial) return routes[name].partial;
+    routes[name].partial = fs.readFileSync(pth.join("views", name + ".html"), "utf8");
+    return routes[name].partial;
+}
+function navigate (route, data, $link) {
+    var partial = loadPartial(route);
+    if (!partial) alert("Broken link!"); // XXX better error dialog
+    $app.html(partial);
+    if (routes[route].onload) routes[route].onload(data);
+    if ($link && $link.parent("li")) {
+        $(".active").removeClass("active");
+        $link.parent("li").addClass("active");
+    }
 }
 
-function loadPartial (name, cb) {
-    console.log("loading partial " + name + ", " + document.location);
-    if (partials[name]) setView(partials[name], cb);
-    $.get(name + ".html", function (html) {
-        console.log("got html");
-        partials[name] = html;
-        setView(partials[name], cb);
-    });
-}
-
-// load scraper UI
-function showScraper () {
-    console.log("showing scraper");
-    loadPartial("scraper", function () {
-        console.log("result from loadPartial");
-        scrapers.listScrapers(function (err, names) {
-            // XXX need a friendly alert dialog instead
-            if (err) return alert(err);
-            var $scraperSel = $("#scraper");
-            $scraperSel.empty();
-            $.each(names, function (i, n) {
-                $("<option></option>")
-                    .attr("value", n)
-                    .text(n.replace(/\.json$/, ""))
-                    .appendTo($scraperSel)
-                ;
-            });
+// the scraper UI
+routes.scraper.onload = function () {
+    scrapers.listScrapers(function (err, names) {
+        // XXX need a friendly alert dialog instead
+        if (err) return alert(err);
+        var $scraperSel = $("#scraper");
+        $scraperSel.empty();
+        $.each(names, function (i, n) {
+            $("<option></option>")
+                .attr("value", n)
+                .text(n.replace(/\.json$/, ""))
+                .appendTo($scraperSel)
+            ;
         });
     });
-}
-
-// load documents UI
-function showDocuments () {
-    loadPartial("documents");
-}
-
-// load JournalTOCs UI
-function showJTOCs () {
-    loadPartial("journal-tocs");
-}
-
-
-
+};
 
 // routing - let's get this party started!
 $(function () {
-    console.log("starting up");
-    console.log(document.location);
     $app = $("#app");
-    page("/", showScraper);
-    page("/documents", showDocuments);
-    page("/journal-tocs", showJTOCs);
-    page();
-    page("/");
+    $("body").on("click", "a.navigate", function (ev) {
+        var $a = $(ev.currentTarget)
+        ,   href = $a.attr("href")
+        ,   data = ev.currentTarget.dataSet || {}
+        ;
+        ev.preventDefault();
+        navigate(href, data, $a);
+    });
+    navigate("scraper", {}, $("ul.nav > li > a[href='scraper']"));
 });
